ba317da1b8ffcafcd782ff450b1d4cd5dd3c8bf0
Riemer van Rozen <R.A.van.Rozen@cwi.nl>
2014-09-17 07:59:36 +0200

    agd 2014 workshop

---
+++ agd2014/mod6_agd_hitpoints.mm
@@ -0,0 +1,7 @@
+//What would you like to modify?
+Tower
+{
+  pool self at 5
+}
+
+modify //evaluating this modification changes the game
---
+++ agd2014/mod7_agd_firerate.mm
@@ -0,0 +1,11 @@
+//What would you like to modify?
+
+Tower
+{
+  pool fireRate at 250
+  user converter upgradeRate
+  xp --> upgradeRate
+  upgradeRate -100-> fireRate
+}
+
+modify //evaluating this modification changes the game
